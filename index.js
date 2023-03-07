690600ad8bfe9d8e18d5523e73e6f6f7dbf535bd
Joel Flores <j_flores88@outlook.com>
2023-03-06 20:05:40 -0800

    Final code for merge sort

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ const fibsRec = (n) => {
     fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length-2]);
     return fibArr;
 }
+
+//MERGE SORT
+const mergeSort = (arr) => {
+    if(arr.length < 2) return arr
+  
+    const middle = Math.floor(arr.length / 2)
+    const left = arr.slice(0, middle);
+    let right = arr.slice(middle)
+    
+    return sort(mergeSort(left), mergeSort(right));
+};
+  
+const sort = (left, right) => {
+    const arr = [];
+  
+    while(left.length && right.length) {
+      if(left[0] < right[0]) {
+        arr.push(left.shift())
+      }
+      else {
+        arr.push(right.shift())
+      }
+    }
+    return arr.concat(left.slice()).concat(right.slice())
+}
